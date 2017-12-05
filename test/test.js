91c4f96ab77e4ec4f1ebbba2d90f5d7dd9908cea
m8r1x <m8r1x.io@gmail.com>
2017-12-05 14:45:48 +0300

    Updates to match 0.1.3
    - Update Tests
    - Bump up version: 0.1.3
    - Update README

--- test/test.js
+++ test/test.js
@@ -3,39 +3,39 @@ import test from 'ava';
 import haskey from '../';
 
 test('haskey: should return { pass: true, missing: [] }', t => {
-  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 }, ['a', 'b', 'c']);
+  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 })(['a', 'b', 'c']);
   t.true(pass);
   t.is(missing.length, 0);
 });
 
 test('haskey: should return missing keys given an array', t => {
-  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 }, ['a', 'b', 'c', 'd']);
+  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 })(['a', 'b', 'c', 'd']);
   const [key] = missing;
   t.false(pass);
   t.is(key, 'd');
 });
 
 test('haskey: should return { pass: true, missing: [] } given comma separated keys', t => {
-  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 }, 'a, b, c');
+  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 })('a, b, c');
   t.true(pass);
   t.is(missing.length, 0);
 });
 
 test('haskey: should return missing keys given comma separated keys', t => {
-  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 }, 'f, a, b, c');
+  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 })('f, a, b, c');
   const [key] = missing;
   t.false(pass);
   t.is(key, 'f');
 });
 
 test('haskey: should return { pass: true, missing: [] } given string of whitespaced keys', t => {
-  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 }, 'a b c');
+  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 })('a b c');
   t.true(pass);
   t.is(missing.length, 0);
 });
 
 test('haskey: should return missing keys given string of whitespaced keys', t => {
-  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 }, 'a b c d');
+  const { pass, missing } = haskey({ a: 1, b: 2, c: 3 })('a b c d');
   const [key] = missing;
   t.false(pass);
   t.is(key, 'd');
@@ -43,21 +43,21 @@ test('haskey: should return missing keys given string of whitespaced keys', t =>
 
 test('haskey: should throw TypeError when keys param is object', t => {
   const error = t.throws(() => {
-    haskey({ a: 1, b: 2 }, {}), TypeError;
+    haskey({ a: 1, b: 2 })({}), TypeError;
   });
   t.is(error.message, 'keys must be a string or array of strings');
 });
 
 test('haskey: should throw TypeError when keys param is number', t => {
   const error = t.throws(() => {
-    haskey({ a: 1, b: 2 }, 1), TypeError;
+    haskey({ a: 1, b: 2 })(1), TypeError;
   });
   t.is(error.message, 'keys must be a string or array of strings');
 });
 
 test('haskey: should throw TypeError when given invalid js object', t => {
   const error = t.throws(() => {
-    haskey([], 'a b c'), TypeError;
+    haskey([])('a b c'), TypeError;
   });
   t.is(error.message, 'invalid object');
 });
